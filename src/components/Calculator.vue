803c249667c59678e3bf52d163f97cc3ad99cf98
boni <felipeboni@4x4brasil.com.br>
2022-03-10 12:52:30 -0300

    feat: calculator functionalities

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="p-5 bg-calculator" style="max-width: 400px; margin: 50px auto">
+    <div class="previous-calc me-2 pt-4">
+      <transition name="slide">
+        <div class="w-full text-end overflow-auto" v-if="animate">
+          <span class="c-font">
+            {{ calculusValue || 0 }}
+          </span>
+        </div>
+      </transition>
+    </div>
+
     <!-- Calculator Result -->
-    <div class="w-full text-end text-white rounded me-2 pt-5">
+    <div
+      class="w-full text-end text-white rounded me-2 overflow-auto"
+      style="margin-top: -0.75rem"
+    >
       <span class="c-font result">
         {{ calculatorValue || 0 }}
       </span>
@@ -9,12 +22,20 @@
 
     <!-- Calculator buttons -->
     <div class="row justify-content-center">
-      <div class="col-3 mb-1 px-1" v-for="n in calculatorElements" :key="n"
-      :class="{'flex-fill': '0'.includes(n)}"
+      <div
+        class="col-3 mb-1 px-1"
+        v-for="n in calculatorElements"
+        :key="n"
+        :class="{ 'flex-fill': '0'.includes(n) }"
       >
         <div
           class="lead calculator-button text-white m-1"
-          :class="{'bg-orange': ['×','÷', '-', '+', '='].includes(n), 'bg-light': ['AC','±', '%'].includes(n), 'w-100 text-start ps-4': '0'.includes(n)}"
+          :class="{
+            'bg-orange': ['×', '÷', '-', '+', '='].includes(n),
+            'bg-light': ['C', '±', '%'].includes(n),
+            'w-100 text-start ps-4': '0'.includes(n)
+          }"
+          @click="action(n)"
         >
           {{ n }}
         </div>
@@ -30,7 +51,7 @@ export default {
     return {
       calculatorValue: "",
       calculatorElements: [
-        "AC",
+        "C",
         "±",
         "%",
         "÷",
@@ -50,7 +71,72 @@ export default {
         ".",
         "=",
       ],
+      operator: null,
+      previousCalculatorValue: "",
+      previousCalculatorValue: "",
+      calculusValue: "",
+      animate: false,
+      animationDirection: "",
+      animationPlayState: "paused",
     };
+  },
+
+  methods: {
+    action(n) {
+      /* Append value */
+      if (!isNaN(n) || n === ".") {
+        this.calculatorValue += n + "";
+      }
+
+      /* Clear value */
+      if (n === "C") {
+        this.calculatorValue = "";
+        this.animate = false;
+      }
+
+      if (n === "%") {
+        this.calculatorValue = this.calculatorValue / 100 + "";
+      }
+
+      if (["÷", "×", "-", "+"].includes(n)) {
+        if (n === "÷") {
+          n = "/";
+        }
+
+        if (n === "×") {
+          n = "*";
+        }
+
+        this.operator = n;
+        this.previousCalculatorValue = this.calculatorValue;
+        this.calculatorValue = "";
+
+        this.animate = false;
+      }
+      
+      if ("±" === n) {
+
+        this.operator = n;
+        this.previousCalculatorValue = -(this.calculatorValue);
+        this.calculatorValue = -(this.calculatorValue);
+
+        this.animate = false;
+      }
+
+      if (n === "=") {
+        if (this.previousCalculatorValue && this.operator && this.calculatorValue) {
+          this.animate = true;
+
+          this.calculusValue =
+            this.previousCalculatorValue + this.operator + this.calculatorValue;
+
+          this.calculatorValue = eval(this.calculusValue);
+
+          this.previousCalculatorValue = "";
+          this.operator = null;
+        }
+      }
+    },
   },
 };
 </script>
@@ -59,6 +145,19 @@ export default {
 .result {
   font-size: 64px;
   font-weight: lighter;
+}
+
+.calculator-button {
+  background: #313131;
+  font-size: 32px;
+  font-weight: normal;
+  line-height: 2.2em;
+  width: 70px;
+  height: 70px;
+  cursor: pointer;
+  border-radius: 999px;
+  text-align: center;
+  transition: all 200ms ease-out;
 }
 
 .bg-calculator {
@@ -80,24 +179,48 @@ export default {
   font-weight: normal !important;
 }
 
-.bg-orange:hover {
-  background: #bb7606 !important;
-}
-
-.calculator-button {
-  background: #313131;
-  font-size: 32px;
-  font-weight: normal;
-  line-height: 2.2em;
-  width: 70px;
-  height: 70px;
-  cursor: pointer;
-  transition: all 100ms ease-out;
-  border-radius: 999px;
-  text-align: center;
-}
-
 .calculator-button:hover {
-  background: #494949;
+  filter: brightness(125%);
+}
+
+.calculator-button.bg-orange:hover {
+  filter: brightness(75%);
+}
+
+.previous-calc {
+  color: #555;
+  margin-right: 0.75rem !important;
+  height: 50px;
+}
+
+/* Animations */
+.slide-enter-active {
+  animation: slide 700ms ease forwards;
+}
+
+.slide-leave-active {
+  animation: slide 300ms ease forwards reverse;
+}
+
+@keyframes slide {
+  from {
+    transform: translateY(50%);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0%);
+    opacity: 1;
+  }
+}
+
+/* Hide scrollbar for Chrome, Safari and Opera */
+*::-webkit-scrollbar {
+  display: none !important;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+* {
+  -ms-overflow-style: none !important; /* IE and Edge */
+  scrollbar-width: none !important; /* Firefox */
 }
 </style>
